a39659a3ce451ad040c74648ce85a46af1cd936c
Danna60635 <1301235924@s.tusd1.org>
2026-05-05 21:57:55 +0000

    Progress

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("Script started");
+
+let feedButton = document.getElementById("feed-btn");
+
+let hungerDisplay = document.getElementById("hunger-level");
+
+let petDisplay = document.getElementById("pet");
+
+function feedPet() {
+    console.log("clicked");
+    
+}
+
+let sleepButton = document.getElementById("play-btn");
+
+let eneryDisplay = document.getElementById("energy-level");
+
+
+
+function sleep(){
+    console.log("clicked");
+}
+
+let playButton = document.getElementById("play-btn");
+
+
+function play(){
+    console.log("clicked");
+}
+
