fdf213b0c0aff48a6a13e288bd1512fef01e36be
Danna60635 <1301235924@s.tusd1.org>
2026-05-07 21:57:41 +0000

    Progress

--- script.js
+++ script.js
@@ -1,30 +1,65 @@
 console.log("Script started");
 
 let feedButton = document.getElementById("feed-btn");
-
 let hungerDisplay = document.getElementById("hunger-level");
-
 let petDisplay = document.getElementById("pet");
+
+let currentHunger = 50;
 
 function feedPet() {
     console.log("clicked");
-    
+    currentHunger = currentHunger + 20;
+    if (currentHunger > 100) {
+        currentHunger = 100;
+    }
+
+    hungerDisplay.innerText = currentHunger;
+
+    if (currentHunger >= 80) {
+        petDisplay.innerText = "😸";
+    }
 }
+feedButton.addEventListener("click", feedPet);
+
 
 let sleepButton = document.getElementById("play-btn");
+let energyDisplay = document.getElementById("energy-level");
 
-let eneryDisplay = document.getElementById("energy-level");
+let currentEnergy = 50;
 
-
-
-function sleep(){
+function sleep() {
     console.log("clicked");
+    currentEnergy = currentEnergy + 20;
+    if (currentEnergy > 100) {
+        currentEnergy = 100;
+    }
+
+    energyDisplay.innerText = currentEnergy;
+
+    if (currentEnergy >= 80) {
+       
+    }
+
 }
+sleepButton.addEventListener("click", sleep);
 
 let playButton = document.getElementById("play-btn");
+let happinessDisplay = document.getElementById("happiness-level");
+
+let currentHappiness = 50;
 
 
-function play(){
+function play() {
     console.log("clicked");
+    currentHappiness = currentHappiness + 20;
+    if (currentHappiness > 100) {
+        currentHappiness = 100;
+    }
+
+    happinessDisplay.innerText = currentHappiness;
+
+    if (currentHappiness >= 80) {
+
+    }
 }
 
